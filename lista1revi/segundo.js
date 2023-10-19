955a9a9c5c4c8365aa641379430a34a475c9b75a
madheus <mat.leiria@gmail.com>
2023-10-19 19:21:50 -0300

    19

--- lista1revi/segundo.js
+++ lista1revi/segundo.js
@@ -1,1 +1,14 @@
 const rl = require('readline-sync')
+let Neleitores = Number(rl.question('Digite o Numero de eleitores: '))
+let Vbranco = Number(rl.question('Numero de votos em branco: '))
+let Vnulos = Number(rl.question('Digite o numero de votos nulos: '))
+let Vvalidos = Number(rl.question('Digite os votos validos: '))
+
+let Brancos = (Number(Vbranco) / Number(Neleitores)) * 100
+let nulos = (Number(Vnulos) / Number(Neleitores)) * 100
+let Validos = (Number(Vvalidos) / Number(Neleitores)) * 100
+
+console.log(`Votos em branco ${Brancos}% de ${Neleitores}`)
+console.log(`Votos nulos ${nulos}% de ${Neleitores}`)
+console.log(`Votos em validos ${Validos}% de ${Neleitores}`)
+
